23ee274754faf3b732e8360d2b9c38e483daf148
Irina <92137503+irudigio@users.noreply.github.com>
2021-11-25 16:33:55 -0300

    Update Jenkinsfile2.groovy

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -1,6 +1,6 @@
 pipeline {
   environment {
-    registry = "mguazzardo/curso-jenkins"
+    registry = "irudigio/curso-jenkins"
     registryCredential = 'dockerhub'
   }
  agent any
@@ -9,7 +9,7 @@ pipeline {
     stage('Build Docker Image') {
             steps {
                 script {
-                    app = docker.build("mguazzardo/flaskapi:latest")
+                    app = docker.build("irudigio/flaskapi:latest")
                 }
             }
         }
@@ -18,7 +18,7 @@ pipeline {
       steps{
         
         sh '''
-        docker run -d --name=flaskapi -p 30080:5000 mguazzardo/flaskapi
+        docker run -d --name=flaskapi -p 5000:5000 irudigio/flaskapi
         
            '''
         }
@@ -52,17 +52,7 @@ pipeline {
             }
         }
     
-    stage ('Deploy k8s') {
-      steps {
-        withCredentials([usernamePassword(credentialsId: 'ssh-key', usernameVariable: 'USUARIO', passwordVariable: 'CONTRASENIA')]) {
-          sh '''
-           sshpass -p ${CONTRASENIA} ssh -o StrictHostKeyChecking=no ${USUARIO}@54.153.2.192 'kubectl create deployment flaskapi --image=mguazzardo/flaskapi -n flaskapi'
-          '''
-        }
-        
-        
-      }
-    }
+    
     
     
     }   
